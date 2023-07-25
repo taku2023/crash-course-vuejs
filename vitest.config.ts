62e197721b13ab7b49771d7f8491a104d3b566f1
takuaki mori <t_mori@cosmoroot.co.jp>
2023-07-25 16:18:16 +0900

    set alias '@' for 'src'

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [Vue()],
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
