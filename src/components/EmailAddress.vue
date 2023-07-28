a8f7412520a0cc82b8203f40812d8addf6d23171
takuaki mori <t_mori@cosmoroot.co.jp>
2023-07-28 22:50:21 +0900

    Pass the first component test

---
+++ src/components/EmailAddress.vue
@@ -0,0 +1,20 @@
+<template>
+  <label class="is-error" v-show="!valid">contains only alphabet</label>
+  <input v-model="email" placeholder="youremail@gmail.com" type="email" />
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { EmailAddress } from "@/models/email";
+const email = ref<string>("");
+
+const valid = computed(() => {
+  return new EmailAddress(email.value).isValid;
+});
+</script>
+
+<style scoped>
+.is-error {
+  color: red;
+}
+</style>
