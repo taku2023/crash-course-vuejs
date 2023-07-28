96aa605266f4685aeca115b704dcd341365b36a4
takuaki mori <t_mori@cosmoroot.co.jp>
2023-07-29 00:12:52 +0900

    Pass password component test

---
+++ src/components/Password.vue
@@ -0,0 +1,22 @@
+<template>
+  <label class="is-error" v-show="!valid"
+    >contains alphabets and digits, 6 or more characters</label
+  >
+  <input type="password" v-model="password" />
+</template>
+
+<script setup lang="ts">
+import { Password } from "@/models/password";
+import { ref, computed } from "vue";
+const password = ref<string>("");
+
+const valid = computed(() => {
+  return new Password(password.value);
+});
+</script>
+
+<style scoped>
+.is-error {
+  color: red;
+}
+</style>
