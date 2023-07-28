a527a03d7715ef7191b5881f5b821d0bcd780930
takuaki mori <t_mori@cosmoroot.co.jp>
2023-07-29 00:12:30 +0900

    Password component test

---
+++ test/password.test.ts
@@ -0,0 +1,21 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+import Password from "@/components/Password.vue";
+
+describe("Password component", () => {
+  it("Shows error when input invalid password", async () => {
+    const wrapper = mount(Password);
+    const input = wrapper.find("input[type='password']");
+    await input.setValue("sh0rt");
+    expect(wrapper.find("label").text()).toBe(
+      "contains alphabets and digits, 6 or more characters"
+    );
+  });
+
+  it("shows no error label", async () => {
+    const wrapper = mount(Password);
+    const input = wrapper.find("input[type='password']");
+    await input.setValue("longpassw0rd");
+    expect(wrapper.find("label").isVisible()).toBeFalsy();
+  });
+});
